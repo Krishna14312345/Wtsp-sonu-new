2ad87094ae33e8953791e07bf13d6e091759b23f
Krishna14312345 <sonurajput750017@gmail.com>
2025-07-18 19:50:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server started on http://localhost:${port}`);
+});
